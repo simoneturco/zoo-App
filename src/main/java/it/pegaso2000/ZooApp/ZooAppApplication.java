ec41fc21825644ee65e19393a0cbbda5cfcda566
Simone Turco <simone.turco@pegaso2000.it>
2023-06-27 12:33:51 +0200

    Progetto spring importato, aggiustato pom.xml per versione della dipendecy MySQL driver

---
+++ src/main/java/it/pegaso2000/ZooApp/ZooAppApplication.java
@@ -0,0 +1,13 @@
+package it.pegaso2000.ZooApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/pegaso2000/ZooApp/ZooAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pegaso2000.ZooApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
